b94f60de3f747646d2801094c35848145a5df380
dyernoon <dongyan34550@hotmail.com>
2015-01-24 19:39:27 +0000

    ....

--- src/main/scala/com/rover/sudoku/ChessBoard.scala
+++ src/main/scala/com/rover/sudoku/ChessBoard.scala
@@ -1,74 +1,75 @@
 package com.rover.sudoku
 
-import org.apache.log4j.Logger
-
 import scala.collection.mutable.ArrayBuffer
+import org.apache.log4j._
 
 /**
  * Created by Nan on 15/1/23.
  */
 class ChessBoard {
 
-  private val logger = Logger.getLogger(getClass.getName)
+  val LOG = org.apache.log4j.Logger.getLogger("ChessBoard")
 
   private val board = new ArrayBuffer[Checker](Constant.BOARD_SIZE * Constant.BOARD_SIZE)
   for (i <- 0 until board.length) {
     board(i) = new Checker(Constant.NONE_VALUE, i)
   }
 
-  def initialize(values: Array[(Int, Int)]) = {
+  def initialize(values : Array[(Int, Int)]) = {
     values.map(it => {
-      if (it._1 < board.length) {
+      if(it._1 < board.length) {
         board(it._1) = new Checker(it._2, it._1)
       }
     })
   }
 
-  def initialize(values: Array[Int]) = {
-    logger.info("initialize the chessboard, input values count = " + values.size)
-    board.clear
-    values.foreach(it => {
-      board += new Checker(it, board.size)
+  def update() = {
+    var updatedFlag = false
+    board.filter(it => !it.hasValue).foreach(it => {
+      val row = getRow(it.getRowIndex)
+      updatedFlag = updatedFlag || it.updateInGroup(row)
+
+      if (!it.hasValue) {
+        val col = getColumn(it.getColumnIndex)
+        updatedFlag = updatedFlag || it.updateInGroup(col)
+      }
+
+      if (!it.hasValue) {
+        val rect = getRect(it.getRectIndex)
+        updatedFlag = updatedFlag || it.updateInGroup(rect)
+      }
     })
+
+    updatedFlag
   }
 
-  def copyFrom = {
-    val copy = new ChessBoard
-    for (i <- 0 until board.length) {
-      board(i) = board(i).copyFrom()
-    }
+  def process() = {
+    LOG.info("Original board:")
+    print
 
-    copy
+    LOG.info("Start process...")
+
+    while(update) { print }
   }
 
-  def getAll = { board }
-  def isAllValueBeSet() = {
-    board.filter(it => !it.hasValue).isEmpty
-  }
-  def isConflict = {
-    board.map(it => it.candidateValue.isEmpty && !it.hasValue).contains(true)
+  private def print() = {
+    board.groupBy(it => it.getRowIndex).map(it => {
+      it._2.mkString(",")
+    }).foreach(LOG.info)
+    LOG.info("\n")
   }
 
-  def getRow(rowIndex: Int) = {
+  private def getRow(rowIndex: Int) = {
     val startIndex = rowIndex * Constant.BOARD_SIZE
     board.slice(startIndex, startIndex + Constant.BOARD_SIZE)
   }
 
-  def getColumn(columnIndex: Int) = {
+  private def getColumn(columnIndex : Int) = {
     board.filter(it => it.getColumnIndex == columnIndex)
   }
 
-  def getRect(rectIndex: Int) = {
+  private def getRect(rectIndex: Int) = {
     board.filter(it => it.getRectIndex == rectIndex)
   }
 
-  override def toString = {
-    val sb = new StringBuilder
-    sb.append("\n")
-    for(i <-0 until Constant.BOARD_SIZE) {
-      sb.append(getRow(i).mkString(",")).append("\n")
-    }
-
-    sb.toString
-  }
 }
